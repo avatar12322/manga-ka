48872166dff835c319123edd1dddf055554a7dc5
avatar12322 <61651065+avatar12322@users.noreply.github.com>
2021-12-14 23:45:15 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+    $("nav").hide();
+
+    $("nav").delay(1000).fadeIn(1000);
+
+});
+
+$(document).ready(function() {
+
+    $(".main_popup").hide();
+
+    $(".main_popup").delay(2000).fadeIn(1000);
+
+});
